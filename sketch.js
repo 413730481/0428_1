d062ab4ab01308ece525795d88977cd6e4c3aa75
413730481 <a0912665202@gmail.com>
2025-04-28 11:13:48 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@ let capture;
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // 全視窗畫布
+  background('#ffe5d9'); // 設定背景顏色為 #ffe5d9
   capture = createCapture(VIDEO); // 擷取攝影機影像
   capture.size(windowWidth * 0.8, windowHeight * 0.8); // 設定影像大小為視窗的 80%
   capture.hide(); // 隱藏原始的 HTML 視訊元素
 }
 
 function draw() {
-  background(220);
+  background('#ffe5d9'); // 每次繪製時重設背景顏色
   // 計算影像置中的位置
   let x = (width - capture.width) / 2;
   let y = (height - capture.height) / 2;
